82d2dbff63e13b73a880afaa0f47e3ffa609cbdc
lichao <663622511@qq.com>
2018-01-29 16:13:14 +0800

    2018-1-29

--- src/java/com/test_1_29/test2/T.java
+++ src/java/com/test_1_29/test2/T.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,10 +19,21 @@ public class T {
     private UserAction action;
 
     @Test
-    public void test3() throws Exception{
+    public void test2() throws Exception{
         action.register();
 
     }
 
+
+    @Autowired
+    private ApplicationContext ctx;
+    @org.junit.Test
+    public void test3() throws Exception{
+
+        ctx=new ClassPathXmlApplicationContext("spring-config.xml");
+        com.test_1_29.test2.controller.UserAction action=  ctx.getBean("userAction", com.test_1_29.test2.controller.UserAction.class);
+        action.register();
+
+    }
 
 }
